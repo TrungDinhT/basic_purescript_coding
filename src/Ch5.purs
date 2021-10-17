13c57fcf0e09cbd8016201e8023b62f431ecae45
Trung Dinh <dinhthanhtrung1996@gmail.com>
2021-10-17 22:19:08 +0200

    Write apply function and operator $

--- src/Ch5.purs
+++ src/Ch5.purs
@@ -11,6 +11,11 @@ flip f x y = f y x
 const :: ∀ a b. a -> b -> a
 const x _ = x
 
+apply :: ∀ a b. (a -> b) -> a -> b
+apply f = f
+
+infixr 0 apply as $
+
 test :: Effect Unit
 test = do
-  log (show (flip const 1 2))
+  log $ show $ flip const 1 2
