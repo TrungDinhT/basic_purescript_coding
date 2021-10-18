748d3d3b65480e0babdb26c934937ac50292a37e
Trung Dinh <dinhthanhtrung1996@gmail.com>
2021-10-18 22:16:41 +0200

    Update docs

--- src/Ch5.purs
+++ src/Ch5.purs
@@ -5,6 +5,14 @@ import Prelude (Unit, show, discard)
 import Effect (Effect)
 import Effect.Console (log)
 
+{-
+  Write functions in Data.Function
+    - flip
+    - const
+    - apply and operator $
+    - applyFlipped and operator #
+-}
+
 flip :: ∀ a b c. (a -> b -> c) -> b -> a -> c
 flip f x y = f y x
 
@@ -19,7 +27,13 @@ infixr 0 apply as $
 applyFlipped :: ∀ a b. a -> (a -> b) -> b
 applyFlipped = flip apply
 
+-- infixl is because it's applyFlipped, we want everything to be regrouped on the left
+-- precedence = 1 is because we want it to be executed at last but just before the $ op
 infixl 1 applyFlipped as #
+
+{-
+  Test function
+-}
 
 test :: Effect Unit
 test = do
