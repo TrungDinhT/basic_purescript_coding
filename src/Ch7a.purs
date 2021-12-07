ab5baba30dacfb1d8f64e4758b26d7f4887d5640
TrungDinhT <dinhthanhtrung1996@gmail.com>
2021-12-07 21:14:31 +0100

    Write Ord instance for Maybe (manually)

--- src/Ch7a.purs
+++ src/Ch7a.purs
@@ -1,7 +1,8 @@
 module Ch7a where
 
-import Prelude (Unit, show, discard, ($), (==))
+import Prelude (Unit, show, discard, ($), (==), (<), (>), (<=), (>=))
 import Data.Eq (class Eq)
+import Data.Ord (class Ord, Ordering(..), compare)
 import Effect (Effect)
 import Effect.Console (log)
 
@@ -10,12 +11,19 @@ data Maybe a = Nothing | Just a
 {-
     Write method for type classes
         - Eq for Maybe
+        - Ord for Maybe
 -}
 
 instance eqMaybe :: Eq a => Eq (Maybe a) where
     eq Nothing Nothing = true
     eq (Just x) (Just y) = x == y
     eq _ _ = false
+
+instance ordMaybe :: Ord a => Ord (Maybe a) where
+    compare Nothing Nothing = EQ
+    compare (Just x) (Just y) = compare x y 
+    compare Nothing _ = LT
+    compare _ Nothing = GT
 
 {-
   Test function
@@ -28,3 +36,11 @@ test = do
     log $ show $ Just 5 == Nothing
     log $ show $ Nothing == Just 5
     log $ show $ Nothing == (Nothing :: Maybe Unit)
+    log "------------------"
+    log $ show $ Just 1 < Just 5
+    log $ show $ Just 5 <= Just 5
+    log $ show $ Just 5 > Just 10
+    log $ show $ Just 10 >= Just 10
+    log $ show $ Just 99 > Nothing
+    log $ show $ Just 99 < Nothing
+
