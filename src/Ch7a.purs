3301d246afdab54e8737715f78c0f68032de78a5
TrungDinhT <dinhthanhtrung1996@gmail.com>
2021-12-07 21:04:35 +0100

    Ch7a: write Eq instance for maybe (manually)

---
+++ src/Ch7a.purs
@@ -0,0 +1,30 @@
+module Ch7a where
+
+import Prelude (Unit, show, discard, ($), (==))
+import Data.Eq (class Eq)
+import Effect (Effect)
+import Effect.Console (log)
+
+data Maybe a = Nothing | Just a
+
+{-
+    Write method for type classes
+        - Eq for Maybe
+-}
+
+instance eqMaybe :: Eq a => Eq (Maybe a) where
+    eq Nothing Nothing = true
+    eq (Just x) (Just y) = x == y
+    eq _ _ = false
+
+{-
+  Test function
+-}
+
+test :: Effect Unit
+test = do
+    log $ show $ Just 5 == Just 5
+    log $ show $ Just 5 == Just 2
+    log $ show $ Just 5 == Nothing
+    log $ show $ Nothing == Just 5
+    log $ show $ Nothing == (Nothing :: Maybe Unit)
