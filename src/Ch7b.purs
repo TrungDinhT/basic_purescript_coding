9615cf0f09a7ab53c206cf462615f92397730b0c
Trung Dinh <dinhthanhtrung1996@gmail.com>
2021-12-12 01:04:27 +0100

    Implementation and test for instance of ToCSV with Person data type

--- src/Ch7b.purs
+++ src/Ch7b.purs
@@ -5,6 +5,9 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
+
 {-
  - The idea here is simply that we want to encode any kind of data into Comma-Separated-Values format, which is
     - primitives -> string
@@ -16,6 +19,43 @@ newtype CSV = CSV String
 class ToCSV a where
     toCSV :: a -> CSV
 
+newtype FullName = FullName String
+
+newtype Age = Age Int
+
+data Occupation = Doctor | Dentist | Lawyer | Unemployed
+
+data Person = Person
+    {
+        name :: FullName,
+        age :: Age,
+        occupation :: Occupation
+    }
+
+-- derive newtype instance showFullName :: Show FullName -- this is not desired, because it will print the representation of string: \"<string_content>\"
+instance showFullName :: Show FullName where
+    show (FullName fullname) = fullname
+
+derive newtype instance showAge :: Show Age
+
+derive instance genericOccupation :: Generic Occupation _
+instance showOccupation :: Show Occupation where
+    show = genericShow
+
+instance toCSVPerson :: ToCSV Person where
+    toCSV (Person { name, age, occupation }) = CSV  $ show name <> "," <> show age <> "," <> show occupation
+
+--Test Data
+me :: Person
+me = Person {
+    name: FullName "Trung",
+    age: Age 25,
+    occupation: Doctor
+}
+
+--Test helpers
+derive newtype instance eqCSV :: Eq CSV
+
 test :: Effect Unit
 test = do
-    log "placeholder"
+    log $ show $ toCSV me == CSV "Trung,25,Doctor"
