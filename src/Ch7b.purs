2dd17a23f0f974838bb6106c85a9c22acbb596d9
Trung Dinh <dinhthanhtrung1996@gmail.com>
2021-12-12 16:31:47 +0100

    FromCSV Typeclass and test

--- src/Ch7b.purs
+++ src/Ch7b.purs
@@ -7,6 +7,8 @@ import Effect.Console (log)
 
 import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
+import Data.Int (fromString)
+import Data.String (Pattern(..), split)
 
 {-
  - The idea here is simply that we want to encode any kind of data into Comma-Separated-Values format, which is
@@ -45,6 +47,29 @@ instance showOccupation :: Show Occupation where
 instance toCSVPerson :: ToCSV Person where
     toCSV (Person { name, age, occupation }) = CSV  $ show name <> "," <> show age <> "," <> show occupation
 
+-- FromCSV Typeclass
+class FromCSV a where
+    fromCSV :: CSV -> Maybe a
+
+toOccupation :: String -> Maybe Occupation
+toOccupation "Doctor" = Just Doctor
+toOccupation "Dentist" = Just Dentist
+toOccupation "Lawyer" = Just Lawyer
+toOccupation "Unemployed" = Just Unemployed
+toOccupation _ = Nothing
+
+instance fromCSVPerson :: FromCSV Person where
+    fromCSV (CSV str) = case split (Pattern ",") str of
+        [fullname, age, occupation] -> go' (fromString age) (toOccupation occupation) where
+            go' Nothing _ = Nothing
+            go' _ Nothing = Nothing
+            go' (Just ageValue) (Just occupationValue) = Just $ Person {
+                name: FullName fullname,
+                age: Age ageValue,
+                occupation: occupationValue
+            }
+        _ -> Nothing
+
 --Test Data
 me :: Person
 me = Person {
@@ -56,6 +81,12 @@ me = Person {
 --Test helpers
 derive newtype instance eqCSV :: Eq CSV
 
+derive newtype instance eqFullName :: Eq FullName
+derive newtype instance eqAge :: Eq Age
+derive instance eqOccupation :: Eq Occupation
+derive instance eqPerson :: Eq Person
+
 test :: Effect Unit
 test = do
     log $ show $ toCSV me == CSV "Trung,25,Doctor"
+    log $ show $ (toCSV me # fromCSV) == Just me
