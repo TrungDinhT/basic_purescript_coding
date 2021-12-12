86335b02355bd2e6cf8e7c4f21ecfd8538412d13
Trung Dinh <dinhthanhtrung1996@gmail.com>
2021-12-12 01:03:10 +0100

    CSV type to be encoded

--- src/Ch7b.purs
+++ src/Ch7b.purs
@@ -5,6 +5,14 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+{-
+ - The idea here is simply that we want to encode any kind of data into Comma-Separated-Values format, which is
+    - primitives -> string
+    - record -> separated values per field (only for field which is not another composite type)
+    - array, list ??
+-}
+newtype CSV = CSV String
+
 test :: Effect Unit
 test = do
     log "placeholder"
