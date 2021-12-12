5d1fb98f370664ffc467ce43b2732d5bbe54d7d3
Trung Dinh <dinhthanhtrung1996@gmail.com>
2021-12-12 16:51:00 +0100

    Cleanup codes for Ch7b

--- src/Ch7b.purs
+++ src/Ch7b.purs
@@ -7,7 +7,6 @@ import Effect.Console (log)
 
 import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
-import Data.Newtype (class Newtype)
 import Data.Maybe (Maybe(..))
 import Data.Int (fromString)
 import Data.String (Pattern(..), split)
@@ -18,16 +17,37 @@ import Data.String (Pattern(..), split)
     - record -> separated values per field (only for field which is not another composite type)
     - array, list ??
 -}
-newtype CSV = CSV String
 
+-- Defining CSV as a newtype for String
+newtype CSV = CSV String
+derive newtype instance eqCSV :: Eq CSV
+derive newtype instance showCSV :: Show CSV
+
+-- ToCSV Typeclass to encode type a to CSV
 class ToCSV a where
     toCSV :: a -> CSV
 
+-- FromCSV Typeclass to decode CSV into type a
+-- This can be failed, so the method returns Maybe a
+class FromCSV a where
+    fromCSV :: CSV -> Maybe a
+
+-------------------------------------- Test type: Person --------------------------------------
 newtype FullName = FullName String
+derive newtype instance eqFullName :: Eq FullName
+-- derive newtype instance showFullName :: Show FullName -- this is not desired, because it will print the representation of string: \"<string_content>\"
+instance showFullName :: Show FullName where
+    show (FullName fullname) = fullname
 
 newtype Age = Age Int
+derive newtype instance eqAge :: Eq Age
+derive newtype instance showAge :: Show Age
 
 data Occupation = Doctor | Dentist | Lawyer | Unemployed
+derive instance eqOccupation :: Eq Occupation
+derive instance genericOccupation :: Generic Occupation _
+instance showOccupation :: Show Occupation where
+    show = genericShow
 
 data Person = Person
     {
@@ -35,24 +55,13 @@ data Person = Person
         age :: Age,
         occupation :: Occupation
     }
+derive instance eqPerson :: Eq Person
 
--- derive newtype instance showFullName :: Show FullName -- this is not desired, because it will print the representation of string: \"<string_content>\"
-instance showFullName :: Show FullName where
-    show (FullName fullname) = fullname
-
-derive newtype instance showAge :: Show Age
-
-derive instance genericOccupation :: Generic Occupation _
-instance showOccupation :: Show Occupation where
-    show = genericShow
-
+-------------------------------------- Implementation of toCSV instance for Person --------------------------------------
 instance toCSVPerson :: ToCSV Person where
     toCSV (Person { name, age, occupation }) = CSV  $ show name <> "," <> show age <> "," <> show occupation
 
--- FromCSV Typeclass
-class FromCSV a where
-    fromCSV :: CSV -> Maybe a
-
+-------------------------------------- Implementation of fromCSV instance for Person --------------------------------------
 toOccupation :: String -> Maybe Occupation
 toOccupation "Doctor" = Just Doctor
 toOccupation "Dentist" = Just Dentist
@@ -72,28 +81,14 @@ instance fromCSVPerson :: FromCSV Person where
             }
         _ -> Nothing
 
---Test helpers
-derive newtype instance eqCSV :: Eq CSV
-
-derive newtype instance eqFullName :: Eq FullName
-derive newtype instance eqAge :: Eq Age
-derive instance eqOccupation :: Eq Occupation
-derive instance eqPerson :: Eq Person
-
-derive instance genericPerson :: Generic Person _
-instance showPerson :: Show Person where
-    show = genericShow
-
-derive instance newtypeCSV :: Newtype CSV _
-derive newtype instance showCSV :: Show CSV
-
+-------------------------------------- Test codes --------------------------------------
 test :: Effect Unit
 test = do
     let me = Person {
-        name: FullName "Trung",
+        name: FullName "Thanh Trung Dinh",
         age: Age 25,
         occupation: Doctor
     }
     log $ show $ toCSV me
-    log $ show $ toCSV me == CSV "Trung,25,Doctor"
+    log $ show $ toCSV me == CSV "Thanh Trung Dinh,25,Doctor"
     log $ show $ (toCSV me # fromCSV) == Just me
