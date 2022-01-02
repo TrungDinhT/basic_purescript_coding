154a7ae8ed838ac1d10eaff2b8c30df4017b632e
Trung Dinh <dinhthanhtrung1996@gmail.com>
2022-01-02 11:13:08 +0100

    Write Semigroup Typeclass

--- src/Ch9.purs
+++ src/Ch9.purs
@@ -4,6 +4,21 @@ import Prelude (Unit, ($), show)
 
 import Effect (Effect)
 import Effect.Console (log)
+-- Semigroup Typeclass
+class Semigroup a where
+    append :: a -> a -> a
+
+{-
+-- Thought: append is infixr, because:
+--  - if it's infix, when having multiple <>, we need to precise where to be executed first with (), 
+--  which is not correct for Semigroup where the operator is associative
+--  - if it's infixl, we have the tendence to think that it's executed from left to right, which is not clear
+--  to show that it's associative??
+--  => We use infixr ??
+-}
+infixr 5 append as <>
+
+
 
 test :: Effect Unit
 test = do
