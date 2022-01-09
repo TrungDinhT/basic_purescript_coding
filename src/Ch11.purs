396824f1cb93acd8a0f265cd20df7bb5e1ea93d6
Trung Dinh <dinhthanhtrung1996@gmail.com>
2022-01-09 16:55:56 +0100

    Write reverse function with fold

--- src/Ch11.purs
+++ src/Ch11.purs
@@ -1,11 +1,18 @@
 module Ch11 where
 
+import Data.List (List(..), foldl, (:))
 import Effect (Effect)
 import Effect.Console (log)
 import Prelude (Unit, show, discard, type (~>), ($))
+
+
+-- reverse with foldl (because we want to pass the new list as the State to next recursive)
+reverse :: List ~> List
+reverse = foldl (\rl x -> x : rl) Nil 
 
 
 -- Test codes
 test :: Effect Unit
 test = do
     log $ show $ "Chap 11: Coding Folds"
+    log $ show $ reverse (30 : 20 : 10 : Nil)
