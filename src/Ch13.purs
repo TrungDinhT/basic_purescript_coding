272b0fdfc70f593f7b528a71baede943f007f793
Trung Dinh <dinhthanhtrung1996@gmail.com>
2022-02-15 21:55:50 +0100

    Write Functor instance for Maybe data type and test code

--- src/Ch13.purs
+++ src/Ch13.purs
@@ -1,9 +1,13 @@
 module Ch13 where
 
+import Data.Generic.Rep (class Generic)
+import Data.Show (class Show)
+import Data.Show.Generic (genericShow)
 import Data.Unit (Unit)
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (show, ($))
+import Prelude (discard, show, ($), (/))
+
 
 -- Functor Typeclass
 class Functor f where
@@ -12,7 +16,22 @@ class Functor f where
 infixl 4 map as <$>
 
 
+-- Maybe data type
+data Maybe a = Nothing | Just a
+derive instance genericMaybe :: Generic (Maybe a) _
+instance showMaybe :: Show a => Show (Maybe a) where
+    show = genericShow
+
+
+-- Functor for Maybe
+instance maybeFunctor :: Functor Maybe where
+    map _ Nothing = Nothing
+    map f (Just x) = Just $ f x 
+
+
 -- Test codes
 test :: Effect Unit
 test = do
     log $ show $ "Chap 13: Coding Functors"
+    log $ show $ (_ / 2) <$> Just 10
+    log $ show $ (_ / 2) <$> Nothing
