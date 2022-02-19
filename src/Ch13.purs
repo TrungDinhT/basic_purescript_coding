bdd98b9aff084c2be5338d23a29bedaddfd535df
Trung Dinh <dinhthanhtrung1996@gmail.com>
2022-02-19 18:25:01 +0100

    Write Functor for Either data type

--- src/Ch13.purs
+++ src/Ch13.purs
@@ -16,10 +16,18 @@ class Functor f where
 infixl 4 map as <$>
 
 
+
 -- Maybe data type
 data Maybe a = Nothing | Just a
 derive instance genericMaybe :: Generic (Maybe a) _
 instance showMaybe :: Show a => Show (Maybe a) where
+    show = genericShow
+
+
+-- Either data type
+data Either a b = Left a | Right b
+derive instance genericEither :: Generic (Either a b) _
+instance showEither :: (Show a, Show b) => Show (Either a b) where
     show = genericShow
 
 
@@ -29,9 +37,17 @@ instance maybeFunctor :: Functor Maybe where
     map f (Just x) = Just $ f x 
 
 
+-- Functor for Either
+instance eitherFunctor :: Functor (Either a) where
+    map _ (Left err) = Left err
+    map f (Right x) = Right $ f x
+
+
 -- Test codes
 test :: Effect Unit
 test = do
     log $ show $ "Chap 13: Coding Functors"
     log $ show $ (_ / 2) <$> Just 10
     log $ show $ (_ / 2) <$> Nothing
+    log $ show $ (_ / 2) <$> (Right 10 :: Either Unit _)
+    log $ show $ (_ / 2) <$> Left "error reason"
